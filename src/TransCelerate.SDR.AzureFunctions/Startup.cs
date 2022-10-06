27c7f28bc0e77f08ae60d2fc7afeeb142bed471f
DIR\viswesh.mb <viswesh.mb@accenture.com>
2022-10-06 13:57:02 +0530

    Added Keyvault for the mongodb db connection string.

--- src/TransCelerate.SDR.AzureFunctions/Startup.cs
+++ src/TransCelerate.SDR.AzureFunctions/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Driver;
 using System;
@@ -10,6 +11,9 @@ using System.Threading.Tasks;
 using TransCelerate.SDR.Core.Utilities;
 using TransCelerate.SDR.Core.Utilities.Common;
 using TransCelerate.SDR.AzureFunctions.DataAccess;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.WebJobs;
 
 [assembly: FunctionsStartup(typeof(TransCelerate.SDR.AzureFunctions.Startup))]
 namespace TransCelerate.SDR.AzureFunctions
@@ -18,9 +22,18 @@ namespace TransCelerate.SDR.AzureFunctions
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            Config.ConnectionString = System.Environment.GetEnvironmentVariable("ConnectionStrings:ServerName");
-            Config.DatabaseName = System.Environment.GetEnvironmentVariable("ConnectionStrings:DatabaseName");
-            
+
+            var vaultName = System.Environment.GetEnvironmentVariable("KeyVaultName");
+            var config = new ConfigurationBuilder().AddEnvironmentVariables();
+            var azureTokenProvider = new AzureServiceTokenProvider();
+            var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback
+                                     (azureTokenProvider.KeyVaultTokenCallback));
+            config.AddAzureKeyVault(vaultName, keyVaultClient, new DefaultKeyVaultSecretManager());
+            var buildConfig = config.Build();
+
+            Config.ConnectionString = Convert.ToString(buildConfig["ConnectionStrings:ServerName"]);
+            Config.DatabaseName = Convert.ToString(buildConfig["ConnectionStrings:DatabaseName"]);                       
+
             builder.Services.AddTransient<IMessageProcessor, MessageProcessor>();
             builder.Services.AddTransient<ILogHelper, LogHelper>();
             builder.Services.AddTransient<IChangeAuditReposotory, ChangeAuditReposotory>();
