a2fd34c002d2974bf5a06467b85850721ce14d36
amrtanshu.gautam <amrtanshu.gautam@BDC3-LX-7WFF2B3>
2022-12-06 19:10:23 +0530

    [Bug 599] :Reference Integrity message for treatment & variableOf Interest shows studyWorkflows in JSON path instead of studyEstimands

--- src/TransCelerate.SDR.Core/Utilities/Common/Constants.cs
+++ src/TransCelerate.SDR.Core/Utilities/Common/Constants.cs
@@ -107,7 +107,7 @@ namespace TransCelerate.SDR.Core.Utilities.Common
 
             public const string InValidDays = "Invalid Date Range";
 
-            public const string BooleanValidationFailed = "Field is not boolean";
+            public const string BooleanValidationFailed = "Type must be boolean (true/false)";
         }
 
         public struct ErrorMessages
