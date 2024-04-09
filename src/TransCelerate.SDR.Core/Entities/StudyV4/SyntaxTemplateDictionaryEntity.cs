9e76cbbcbc3027ce70b97bafd02e2ec3cfb172e9
InduSekharSajja <94108304+InduSekharSajja@users.noreply.github.com>
2024-04-09 16:01:37 +0530

    Code Fixes for Bugs#923

--- src/TransCelerate.SDR.Core/Entities/StudyV4/SyntaxTemplateDictionaryEntity.cs
+++ src/TransCelerate.SDR.Core/Entities/StudyV4/SyntaxTemplateDictionaryEntity.cs
@@ -17,7 +17,7 @@ namespace TransCelerate.SDR.Core.Entities.StudyV4
         public string Label { get; set; }
         public string Description { get; set; }
         public string Text { get; set; }        
-        public List<ParameterMapDto> ParameterMap { get; set; }
+        public List<ParameterMapDto> ParameterMaps { get; set; }
         public string InstanceType { get; set; }
     }
 }
