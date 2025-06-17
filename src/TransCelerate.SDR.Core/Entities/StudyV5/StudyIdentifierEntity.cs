a5a5706a60b5379a1e84c82bc6d4d01bc4d7ea8b
Jon Masson <jonathan.masson@capgemini.com>
2025-06-17 09:18:05 +0100

    StudyIdentifier inherits from Identifier

--- src/TransCelerate.SDR.Core/Entities/StudyV5/StudyIdentifierEntity.cs
+++ src/TransCelerate.SDR.Core/Entities/StudyV5/StudyIdentifierEntity.cs
@@ -1,14 +1,7 @@
 ﻿namespace TransCelerate.SDR.Core.Entities.StudyV5
 {
     [MongoDB.Bson.Serialization.Attributes.BsonNoId]
-    public class StudyIdentifierEntity : IId
+    public class StudyIdentifierEntity : IdentifierEntity
     {        
-        public string Id { get; set; }
-        public string Text { get; set; }
-        public OrganizationEntity Scope { get; set; }
-        public string InstanceType { get; set; }
     }
 }
-// Study identifier should inherit from IdentifierEntity - this will give it scope and text automatically. 
-// StudyIdentifierScope becomes scope - this is used everywhere, so rename it to scope first, and then change the inheritance
-// Also rename studyIdentifier to Text first
