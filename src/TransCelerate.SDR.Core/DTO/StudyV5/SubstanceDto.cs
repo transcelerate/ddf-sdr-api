377f8b1471138f9df2ab1a6a261ab4af23015ff2
Jon Masson <jonathan.masson@capgemini.com>
2025-06-17 11:02:06 +0100

    Strength and substance classes

---
+++ src/TransCelerate.SDR.Core/DTO/StudyV5/SubstanceDto.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace TransCelerate.SDR.Core.DTO.StudyV5
+{
+    public class SubstanceDto : IId
+    {
+        public string Id { get; set; }
+        public string InstanceType { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public string Label { get; set; }
+        public SubstanceDto ReferenceSubstance { get; set; }
+        public List<CodeDto> Codes { get; set; }
+    }
+}
---
+++ src/TransCelerate.SDR.Core/Entities/StudyV5/SubstanceEntity.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace TransCelerate.SDR.Core.Entities.StudyV5
+{
+    [MongoDB.Bson.Serialization.Attributes.BsonIgnoreExtraElements]
+    [MongoDB.Bson.Serialization.Attributes.BsonNoId]
+    public class SubstanceEntity : IId
+    {
+        public string Id { get; set; }
+        public string InstanceType { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Label { get; set; }
+        public SubstanceEntity ReferenceSubstance { get; set; }
+        public List<CodeEntity> Codes { get; set; }
+    }
+}
