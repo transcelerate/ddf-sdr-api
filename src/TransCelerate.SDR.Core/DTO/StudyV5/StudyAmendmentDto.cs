963e857cbe52d59d34a99fe143e78671a7840855
Jon Masson <jonathan.masson@capgemini.com>
2025-06-20 11:24:13 +0100

    Update StudyAmendment class

--- src/TransCelerate.SDR.Core/DTO/StudyV5/StudyAmendmentDto.cs
+++ src/TransCelerate.SDR.Core/DTO/StudyV5/StudyAmendmentDto.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace TransCelerate.SDR.Core.DTO.StudyV5
 {
     public class StudyAmendmentDto : IId
     {
         public string Id { get; set; }
+        public string InstanceType { get; set; }
         public string Number { get; set; }
         public string Summary { get; set; }
-        public bool SubstantialImpact { get; set; }
-        public string PreviousId { get; set; }
+        public List<GovernanceDateDto> DateValues { get; set; }
+        public List<GeographicScopeDto> GeographicScopes { get; set; }
+        public List<StudyChangeDto> Changes { get; set; }
+        public List<StudyAmendmentImpact> Impacts { get; set; }
         public List<SubjectEnrollmentDto> Enrollments { get; set; }
+        public string PreviousId { get; set; }
         public StudyAmendmentReasonDto PrimaryReason { get; set; }
         public List<StudyAmendmentReasonDto> SecondaryReasons { get; set; }
-        public string InstanceType { get; set; }
+        public List<CommentAnnotationDto> Notes { get; set; }
     }
 }
