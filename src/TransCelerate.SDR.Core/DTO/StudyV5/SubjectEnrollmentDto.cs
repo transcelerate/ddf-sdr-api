6fa92f1f43e1f3448ddc5aeb396c419230b74cee
Jon Masson <jonathan.masson@capgemini.com>
2025-06-20 11:20:32 +0100

    Update subjectEnrollment class

--- src/TransCelerate.SDR.Core/DTO/StudyV5/SubjectEnrollmentDto.cs
+++ src/TransCelerate.SDR.Core/DTO/StudyV5/SubjectEnrollmentDto.cs
@@ -1,7 +1,16 @@
-﻿namespace TransCelerate.SDR.Core.DTO.StudyV5
+﻿using System.Collections.Generic;
+
+namespace TransCelerate.SDR.Core.DTO.StudyV5
 {
-    public class SubjectEnrollmentDto : GeographicScopeDto
+    public class SubjectEnrollmentDto : IId
     {
+        public string Id { get; set; }
+        public string InstanceType { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Label { get; set; }
         public QuantityDto Quantity { get; set; }
+        public List<GeographicScopeDto> GeographicScopes { get; set; } 
+        public StudySiteDto StudySite { get; set; }   
     }
 }
--- src/TransCelerate.SDR.Core/Entities/StudyV5/SubjectEnrollmentEntity.cs
+++ src/TransCelerate.SDR.Core/Entities/StudyV5/SubjectEnrollmentEntity.cs
@@ -1,7 +1,16 @@
-﻿namespace TransCelerate.SDR.Core.Entities.StudyV5
+﻿using System.Collections.Generic;
+
+namespace TransCelerate.SDR.Core.Entities.StudyV5
 {
-    public class SubjectEnrollmentEntity : GeographicScopeEntity
+    public class SubjectEnrollmentEntity : IId
     {
-        public QuantityEntity Quantity { get; set; }        
+        public string Id { get; set; }
+        public string InstanceType { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Label { get; set; }
+        public QuantityEntity Quantity { get; set; }
+        public List<GeographicScopeEntity> GeographicScopes { get; set; } 
+        public StudySiteEntity StudySite { get; set; }   
     }
 }
