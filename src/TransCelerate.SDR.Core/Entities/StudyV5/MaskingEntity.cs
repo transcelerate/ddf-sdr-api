d80e1ce0f88d0e07139f54d78429db5495e34199
Jon Masson <jonathan.masson@capgemini.com>
2025-06-27 12:11:43 +0100

    Masking updates

--- src/TransCelerate.SDR.Core/Entities/StudyV5/MaskingEntity.cs
+++ src/TransCelerate.SDR.Core/Entities/StudyV5/MaskingEntity.cs
@@ -3,9 +3,10 @@
     [MongoDB.Bson.Serialization.Attributes.BsonIgnoreExtraElements]
     [MongoDB.Bson.Serialization.Attributes.BsonNoId]
     public class MaskingEntity : IId
-    {        
+    {
         public string Id { get; set; }
-        public string Description { get; set; }
+        public string Text { get; set; }
         public string InstanceType { get; set; }
+        public bool IsMasked { get; set; }
     }
 }
