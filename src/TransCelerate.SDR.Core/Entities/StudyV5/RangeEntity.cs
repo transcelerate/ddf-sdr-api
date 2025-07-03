644a74701a4b7e2cdda94b9432e73ad3a692f1de
Jon Masson <jonathan.masson@capgemini.com>
2025-07-03 14:50:24 +0100

    QuantityRange class

--- src/TransCelerate.SDR.Core/Entities/StudyV5/RangeEntity.cs
+++ src/TransCelerate.SDR.Core/Entities/StudyV5/RangeEntity.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TransCelerate.SDR.Core.Entities.StudyV5
+﻿namespace TransCelerate.SDR.Core.Entities.StudyV5
 {
-    public class RangeEntity : IId
-    {
-        public string Id { get; set; }        
+    public class RangeEntity : QuantityRangeEntity
+    {      
         public int MinValue { get; set; }
         public int MaxValue { get; set; }
         public CodeEntity Unit { get; set; }
         public bool IsApproximate { get; set; }
-        public string InstanceType { get; set; }
     }
 }
