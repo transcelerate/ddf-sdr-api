4821b80c92c5fb976ffd32303e72b577cdda3773
Jon Masson <jonathan.masson@capgemini.com>
2025-07-08 09:42:57 +0100

    Population definition updates

--- src/TransCelerate.SDR.RuleEngine/StudyV5Rules/StudyCohortValidator.cs
+++ src/TransCelerate.SDR.RuleEngine/StudyV5Rules/StudyCohortValidator.cs
@@ -54,14 +54,14 @@ namespace TransCelerate.SDR.RuleEngineV5
                .NotNull().WithMessage(Constants.ValidationErrorMessage.PropertyMissingError)
                .NotEmpty().WithMessage(Constants.ValidationErrorMessage.PropertyEmptyError)
                .When(x => RulesHelper.GetConformanceRules(_httpContextAccessor.HttpContext.Request.Headers[IdFieldPropertyName.Common.UsdmVersion], nameof(StudyCohortValidator), nameof(StudyCohortDto.PlannedEnrollmentNumber)), ApplyConditionTo.AllValidators)
-               .SetValidator(new RangeValidator(_httpContextAccessor));
+               .SetValidator(new QuantityRangeValidator(_httpContextAccessor));
 
             RuleFor(x => x.PlannedCompletionNumber)
                .Cascade(CascadeMode.Stop)
                .NotNull().WithMessage(Constants.ValidationErrorMessage.PropertyMissingError)
                .NotEmpty().WithMessage(Constants.ValidationErrorMessage.PropertyEmptyError)
                .When(x => RulesHelper.GetConformanceRules(_httpContextAccessor.HttpContext.Request.Headers[IdFieldPropertyName.Common.UsdmVersion], nameof(StudyCohortValidator), nameof(StudyCohortDto.PlannedCompletionNumber)), ApplyConditionTo.AllValidators)
-               .SetValidator(new RangeValidator(_httpContextAccessor));
+               .SetValidator(new QuantityRangeValidator(_httpContextAccessor));
 
             RuleFor(x => x.PlannedAge)
                .Cascade(CascadeMode.Stop)
