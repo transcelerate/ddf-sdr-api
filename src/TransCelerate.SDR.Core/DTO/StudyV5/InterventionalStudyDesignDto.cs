7cfb61595cec29b129b858038ad4b1597f5a949c
Jon Masson <jonathan.masson@capgemini.com>
2025-07-14 07:49:04 +0100

    Remove properties that will be inherited

--- src/TransCelerate.SDR.Core/DTO/StudyV5/InterventionalStudyDesignDto.cs
+++ src/TransCelerate.SDR.Core/DTO/StudyV5/InterventionalStudyDesignDto.cs
@@ -4,8 +4,6 @@ namespace TransCelerate.SDR.Core.DTO.StudyV5
 {
     public class InterventionalStudyDesignDto : StudyDesignDto
     {
-        public AliasCodeDto StudyPhase { get; set; }
-        public CodeDto StudyType { get; set; }
         public List<CodeDto> SubTypes { get; set; }
         public List<CodeDto> IntentTypes { get; set; }
         public CodeDto Model { get; set; }
