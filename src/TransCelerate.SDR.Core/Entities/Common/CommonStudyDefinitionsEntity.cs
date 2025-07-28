c5b51a9c03f3067f008c8cdf40227c5d8c5e2e91
Phyu Han <phyu.han@capgemini.com>
2025-07-28 12:04:52 -0400

    Refactor common study definitions entity v5

--- src/TransCelerate.SDR.Core/Entities/Common/CommonStudyDefinitionsEntity.cs
+++ src/TransCelerate.SDR.Core/Entities/Common/CommonStudyDefinitionsEntity.cs
@@ -1,9 +1,10 @@
 ﻿
+using MongoDB.Bson.Serialization.Attributes;
 using System.Collections.Generic;
 
 namespace TransCelerate.SDR.Core.Entities.Common
 {
-    [MongoDB.Bson.Serialization.Attributes.BsonIgnoreExtraElements]
+    [BsonIgnoreExtraElements]
     public class CommonStudyDefinitionsEntity
     {
         public CommonStudyEntity Study { get; set; }
@@ -12,8 +13,8 @@ namespace TransCelerate.SDR.Core.Entities.Common
 
     }
 
-    [MongoDB.Bson.Serialization.Attributes.BsonIgnoreExtraElements]
-    [MongoDB.Bson.Serialization.Attributes.BsonNoId]
+    [BsonIgnoreExtraElements]
+    [BsonNoId]
     public class CommonStudyEntity
     {
         public string StudyId { get; set; }
@@ -29,21 +30,34 @@ namespace TransCelerate.SDR.Core.Entities.Common
         public List<CommonStudyVersions> Versions { get; set; }
         public CommonStudyDocumentedBy DocumentedBy { get; set; }
     }
-    
-	[MongoDB.Bson.Serialization.Attributes.BsonIgnoreExtraElements]
-    [MongoDB.Bson.Serialization.Attributes.BsonNoId]
+
+    [BsonIgnoreExtraElements]
+    public class CommonStudyDefinitionsEntityV5 : CommonStudyDefinitionsEntity
+    {
+        public new CommonStudyEntityV5 Study { get; set; }
+    }
+
+    [BsonIgnoreExtraElements]
+    [BsonNoId]
+    public class CommonStudyEntityV5 : CommonStudyEntity
+    {
+        public new List<CommonStudyDocumentedBy> DocumentedBy { get; set; }
+    }
+
+    [BsonIgnoreExtraElements]
+    [BsonNoId]
     public class CommonStudyProtocolVersions
     {
         public string ProtocolVersion { get; set; }
     }
-    [MongoDB.Bson.Serialization.Attributes.BsonNoId]
-    [MongoDB.Bson.Serialization.Attributes.BsonIgnoreExtraElements]
+    [BsonNoId]
+    [BsonIgnoreExtraElements]
     public class CommonStudyDocumentedBy
     {
         public List<CommonStudyProtocolVersions> Versions { get; set; }
     }
-    [MongoDB.Bson.Serialization.Attributes.BsonIgnoreExtraElements]
-    [MongoDB.Bson.Serialization.Attributes.BsonNoId]
+    [BsonIgnoreExtraElements]
+    [BsonNoId]
     public class CommonStudyVersions
     {
         public string Id { get; set; }
@@ -55,8 +69,8 @@ namespace TransCelerate.SDR.Core.Entities.Common
         public List<CommonStudyDesignsV4> StudyDesigns { get; set; }
     }
 
-    [MongoDB.Bson.Serialization.Attributes.BsonIgnoreExtraElements]
-    [MongoDB.Bson.Serialization.Attributes.BsonNoId]
+    [BsonIgnoreExtraElements]
+    [BsonNoId]
     public class CommonStudyTitle
     {
         public string Id { get; set; }
@@ -65,8 +79,8 @@ namespace TransCelerate.SDR.Core.Entities.Common
         public string InstanceType { get; set; }
     }
 
-    [MongoDB.Bson.Serialization.Attributes.BsonIgnoreExtraElements]
-    [MongoDB.Bson.Serialization.Attributes.BsonNoId]
+    [BsonIgnoreExtraElements]
+    [BsonNoId]
     public class CommonStudyDesigns
     {
         public string Id { get; set; }
@@ -75,16 +89,16 @@ namespace TransCelerate.SDR.Core.Entities.Common
         public object InterventionModel { get; set; }
         public List<CommonStudyIndication> StudyIndications { get; set; }
     }
-    [MongoDB.Bson.Serialization.Attributes.BsonIgnoreExtraElements]
-    [MongoDB.Bson.Serialization.Attributes.BsonNoId]
+    [BsonIgnoreExtraElements]
+    [BsonNoId]
     public class CommonStudyDesignsV4
     {
         public string Id { get; set; }
         public object InterventionModel { get; set; }
         public List<CommonStudyIndication> Indications { get; set; }
     }
-    [MongoDB.Bson.Serialization.Attributes.BsonIgnoreExtraElements]
-    [MongoDB.Bson.Serialization.Attributes.BsonNoId]
+    [BsonIgnoreExtraElements]
+    [BsonNoId]
     public class CommonCodeEntity
     {
         public string Id { get; set; }
@@ -93,8 +107,8 @@ namespace TransCelerate.SDR.Core.Entities.Common
         public string CodeSystemVersion { get; set; }
         public string Decode { get; set; }
     }
-    [MongoDB.Bson.Serialization.Attributes.BsonIgnoreExtraElements]
-    [MongoDB.Bson.Serialization.Attributes.BsonNoId]
+    [BsonIgnoreExtraElements]
+    [BsonNoId]
     public class CommonStudyIndication
     {
         public string IndicationDescription { get; set; }
