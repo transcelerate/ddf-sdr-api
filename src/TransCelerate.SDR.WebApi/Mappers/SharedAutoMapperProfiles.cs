92d82e0258ea910819546f0411401806df59187d
Jon Masson <jonathan.masson@capgemini.com>
2025-08-07 08:48:57 +0100

    Identifier Scope cross reference Dto

--- src/TransCelerate.SDR.WebApi/Mappers/SharedAutoMapperProfiles.cs
+++ src/TransCelerate.SDR.WebApi/Mappers/SharedAutoMapperProfiles.cs
@@ -228,7 +228,7 @@ namespace TransCelerate.SDR.WebApi.Mappers
 			CreateMap<CommonStudyIdentifiersDto, Core.DTO.StudyV5.StudyIdentifierDto>()
 				.ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
 				.ForMember(dest => dest.Text, opt => opt.MapFrom(src => src.StudyIdentifier))
-				.ForMember(dest => dest.Scope, opt => opt.MapFrom(src => src.StudyIdentifierScope))
+				.ForMember(dest => dest.ScopeId, opt => opt.MapFrom(src => src.StudyIdentifierScope))
 				.ReverseMap();
 
 
