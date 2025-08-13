f29cc02fc9ab3e41c2af82377fa6f8b19cebf0af
Jon Masson <jonathan.masson@capgemini.com>
2025-08-13 09:28:35 +0100

    Fix circular loop in Substance validator

--- src/TransCelerate.SDR.RuleEngine/StudyV5Rules/SubstanceValidator.cs
+++ src/TransCelerate.SDR.RuleEngine/StudyV5Rules/SubstanceValidator.cs
@@ -62,7 +62,7 @@ namespace TransCelerate.SDR.RuleEngineV5
                 .NotNull().WithMessage(Constants.ValidationErrorMessage.PropertyMissingError)
                 .NotEmpty().WithMessage(Constants.ValidationErrorMessage.PropertyEmptyError)
                 .When(x => RulesHelper.GetConformanceRules(_httpContextAccessor.HttpContext.Request.Headers[IdFieldPropertyName.Common.UsdmVersion], nameof(SubstanceValidator), nameof(SubstanceDto.ReferenceSubstance)), ApplyConditionTo.AllValidators)
-                .SetValidator(new SubstanceValidator(_httpContextAccessor));
+                .SetValidator((substance, referenceSubstance) => new SubstanceValidator(_httpContextAccessor));
 
             RuleFor(x => x.Codes)
                 .Cascade(CascadeMode.Stop)
